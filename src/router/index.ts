f8434d4aad51703702de130aa11c8736bcdbd694
Towerrrr <1611993104@qq.com>
2025-09-20 23:56:53 +0800

    fix: 路由初修

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,16 +6,23 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: '主页',
+      component: HomePage,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/user/login',
+      name: '用户登录',
+      component: UserLoginPage,
+    },
+    {
+      path: '/user/register',
+      name: '用户注册',
+      component: UserRegisterPage,
+    },
+    {
+      path: '/admin/userManage',
+      name: '用户管理',
+      component: UserManagePage,
     },
   ],
 })
