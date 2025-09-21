46eb57bcb09683e5b0a104030987f10b33e758ec
Towerrrr <1611993104@qq.com>
2025-09-21 23:25:18 +0800

    fix: 获取登录用户

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -8,10 +8,12 @@ export const useLoginUserStore = defineStore("loginUser", () => {
 
   async function fetchLoginUser() {
     // todo 由于后端还没提供接口，暂时注释
-    // const res = await getCurrentUser();
-    // if (res.data.code === 0 && res.data.data) {
-    //   loginUser.value = res.data.data;
-    // }
+    async function fetchLoginUser() {
+      const res = await getLoginUserUsingGet()
+      if (res.data.code === 0 && res.data.data) {
+        loginUser.value = res.data.data
+      }
+    }
   }
 
   function setLoginUser(newLoginUser: any) {
