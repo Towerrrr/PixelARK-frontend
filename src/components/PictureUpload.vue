effc0ea27a01231424050a8c721b9ac433741a80
Towerrrr <1611993104@qq.com>
2025-10-18 23:51:16 +0800

    fix: 图片上传组件

--- src/components/PictureUpload.vue
+++ src/components/PictureUpload.vue
@@ -1,21 +1,20 @@
 <template>
+  <div class="picture-upload">
   <a-upload
-    v-model:file-list="fileList"
-    name="avatar"
     list-type="picture-card"
-    class="avatar-uploader"
     :show-upload-list="false"
-    action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+    :custom-request="handleUpload"
     :before-upload="beforeUpload"
-    @change="handleChange"
   >
-    <img v-if="imageUrl" :src="imageUrl" alt="avatar" />
+    <img v-if="picture?.url" :src="picture?.url" alt="avatar" />
     <div v-else>
       <loading-outlined v-if="loading"></loading-outlined>
       <plus-outlined v-else></plus-outlined>
-      <div class="ant-upload-text">Upload</div>
+      <div class="ant-upload-text">点击或拖拽上传图片</div>
     </div>
   </a-upload>
+</div>
+
 </template>
 <script lang="ts" setup>
 import { ref } from 'vue';
